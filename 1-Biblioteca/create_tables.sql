70a6f4e42b54841aa712e3b1c012b74e48547556
Estrada Diego <diegoestrada35@gmail.com>
2024-03-21 22:56:26 -0300

    rediseño schema de la BD

--- 1-Biblioteca/create_tables.sql
+++ 1-Biblioteca/create_tables.sql
@@ -4,21 +4,8 @@ CREATE TABLE   socio(cod_socio integer PRIMARY KEY,
   telefono integer,
   nombre text,
   apellido text,
+  fecha_baja text,
   UNIQUE (dni)
-);
-
-CREATE TABLE   socio_baja(cod_socio integer PRIMARY KEY,
-  dni integer,
-  direccion text,
-  telefono integer,
-  nombre text,
-  apellido text,
-  timestamp_baja current_timestamp NOT NULL DEFAULT CURRENT_DATE,
-  UNIQUE (dni)
-);
-
-CREATE TABLE  estado_prestamo(
-  id_estado text UNIQUE NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE  prestamo(
@@ -26,14 +13,7 @@ CREATE TABLE  prestamo(
   cod_socio integer references socio(cod_socio)
   on DELETE restrict deferrable initially deferred,
   id_estado text references estado_prestamo(id_estado) on DELETE restrict deferrable initially deferred NOT NULL DEFAULT "PENDIENTE"
-);
-
-CREATE TABLE  prestamo_baja(
-  id_prestamo integer NOT NULL PRIMARY KEY,
-  cod_socio integer references socio(cod_socio)
-  on DELETE restrict deferrable initially deferred,
-  id_estado text references estado_prestamo(id_estado) on DELETE restrict deferrable initially deferred,
-  timestamp_baja  current_timestamp NOT NULL DEFAULT CURRENT_DATE
+  fecha_baja text,
 );
 
 CREATE TABLE  item_libro_en_prestamo(
@@ -44,18 +24,9 @@ CREATE TABLE  item_libro_en_prestamo(
   fecha_prestamo NOT NULL DEFAULT CURRENT_DATE,
   fecha_limite_devolucion DEFAULT CURRENT_DATE,
   fecha_devolucion text
+  fecha_baja text,
 );
 
-
-CREATE TABLE  item_libro_en_prestamo_baja(
-  id_item integer PRIMARY KEY,
-  id_libro  integer references libro(id_ejemplar)
-  on DELETE restrict deferrable initially deferred,
-  id_prestamo integer references prestamo(id_prestamo) ON DELETE restrict deferrable initially deferred,
-  fecha_prestamo NOT NULL DEFAULT CURRENT_DATE,
-  fecha_limite_devolucion DEFAULT CURRENT_DATE,
-  fecha_devolucion CURRENT_DATE
-);
 
 CREATE TABLE  libro(
   id_ejemplar integer PRIMARY KEY,
@@ -67,19 +38,7 @@ CREATE TABLE  libro(
   timestamp_registro current_timestamp NOT NULL DEFAULT CURRENT_DATE,
   autores text,
   isbn  integer
-);
-
-CREATE TABLE  libro_baja(
-  id_ejemplar integer PRIMARY KEY,
-  anio  integer,
-  titulo  text,
-  editorial text,
-  estado_fisico text,
-  disponibilidad text NOT NULL DEFAULT "Baja",
-  timestamp_registro current_timestamp,
-  timestamp_baja current_timestamp NOT NULL DEFAULT CURRENT_DATE,
-  autores text,
-  isbn  integer
+  fecha_baja text,
 );
 
 CREATE TRIGGER  fecha_limite AFTER INSERT ON item_libro_en_prestamo
