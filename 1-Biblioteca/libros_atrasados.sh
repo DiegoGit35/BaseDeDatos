709d5bf77312282d315dde84b406291f58b18613
diebo <diegoestrada35@gmail.com>
2024-03-22 03:49:14 -0300

    listar prestamos atrasados

--- 1-Biblioteca/libros_atrasados.sh
+++ 1-Biblioteca/libros_atrasados.sh
@@ -1,1 +1,7 @@
-sqlite3 biblioteca.db "select * from libro where id_ejemplar=(select id_libro from item_libro_en_prestamo where fecha_devolucion=NULL)"
+sqlite3 biblioteca.db "SELECT libro.titulo, socio.nombre, item_libro_en_prestamo.fecha_limite_devolucion 
+FROM libro
+JOIN item_libro_en_prestamo ON libro.id_ejemplar = item_libro_en_prestamo.id_libro
+JOIN prestamo ON item_libro_en_prestamo.id_prestamo = prestamo.id_prestamo
+JOIN socio ON prestamo.cod_socio = socio.cod_socio 
+WHERE item_libro_en_prestamo.fecha_limite_devolucion < current_date;"
+
